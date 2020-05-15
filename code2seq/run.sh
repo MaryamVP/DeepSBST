07ff78b707fd48d29b85385ca2e2a6b84984a9b4
lizhuo-1994 <57395398+lizhuo-1994@users.noreply.github.com>
2020-05-15 13:21:17 +0800

    Update run.sh

--- code2seq/run.sh
+++ code2seq/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3.6 main.py -d /Users/lizhuo/code2seq/data -te /Users/lizhuo/code2seq/data -s /Users/lizhuo/code2seq/data -l /Users/lizhuo/code2seq/model/models/java-large-model/model_iter52.release
+python3.6 main.py -d ./data -te ./data -s ./data -l ./model/models/java-large-model/model_iter52.release
