555204cd46facf3438352995711f1109d92526a6
lizhuo-1994 <57395398+lizhuo-1994@users.noreply.github.com>
2020-05-15 13:17:29 +0800

    Create run.sh

---
+++ code2seq/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3.6 main.py -d /Users/lizhuo/code2seq/data -te /Users/lizhuo/code2seq/data -s /Users/lizhuo/code2seq/data -l /Users/lizhuo/code2seq/model/models/java-large-model/model_iter52.release
